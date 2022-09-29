b1a7f903d930070c7588243f64838ba672fc79f0
Jmart5564 <Jmart5564@gmail.com>
2022-09-29 15:22:09 -0700

    confirmed that user can sign in and sign up, doing so redirects to the todos page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Auth from './components/Auth/Auth';
+import Todos from './components/Todo/Todos';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/auth/:type" component={Auth} />
+        <Route path="/todos" component={Todos} />
         <Route exact path="/">
           <Redirect to="/auth/sign-in" />
         </Route>
---
+++ src/components/Todo/Todos.js
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+
+
+
+export default function Todos() {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <Redirect to='/auth/sign-in' />;
+  }
+
+  return (
+    <div>hello</div>
+  );
+}
