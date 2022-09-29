5ff7b941d217add904d6a0f361503098f916a105
Jmart5564 <Jmart5564@gmail.com>
2022-09-29 15:50:54 -0700

    user can add todos and see a list of their todos

--- src/components/Todo/Todos.js
+++ src/components/Todo/Todos.js
@@ -2,25 +2,48 @@ import { useContext, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
 import { useTodos } from '../../hooks/useTodos';
+import { createTodo } from '../../services/todos';
 
 
 
 export default function Todos() {
   const { user } = useContext(UserContext);
   const [description, setDescription] = useState('');
-  const { items, setItems } = useTodos();
+  const { todos, setTodos } = useTodos();
 
   if (!user) {
     return <Redirect to='/auth/sign-in' />;
   }
 
+  const handleNewTodo = async () => {
+    try {
+      await createTodo(description);
+      setTodos((prev) => [...prev, { description }]);
+      setDescription('');
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e.message);
+    }
+  };
+
   return (
     <div>
-      <input
-        type='text'
-        value={description}
-      />
-      <button>Add Todo</button>
+      <div>
+        <input
+          type='text'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={handleNewTodo}>Add Todo</button>
+      </div>
+      {todos.map((item) => (
+        <div key={item.id}>
+          <label>
+            <input type="checkbox" />
+            {item.description}
+          </label>
+        </div>
+      ))}
     </div>
   );
 }
