12c157a11f01858ad0b932a269af26b0c44d9412
Jmart5564 <Jmart5564@gmail.com>
2022-09-29 12:33:43 -0700

    built out todos service

---
+++ src/services/todos.js
@@ -0,0 +1,22 @@
+import { checkError, client } from "./client";
+
+export async function getTodos() {
+    const response = await client.from('todos').select();
+    return checkError(response);
+}
+
+export async function createTodo(description) {
+    const response = await client.from('todos').insert({ description });
+
+    return checkError(response)
+}
+
+export async function toggleTodo({ id, complete }) {
+    const response = await client
+    .from('todos')
+    .update({ complete: !complete })
+    .match({ id })
+    .single();
+
+    return checkError(response);
+}
