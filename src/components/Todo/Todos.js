f4ef1826c01b9e1fd5dfddb602efe97514181d29
Jmart5564 <Jmart5564@gmail.com>
2022-09-29 15:37:45 -0700

    added input for addTodos

--- src/components/Todo/Todos.js
+++ src/components/Todo/Todos.js
@@ -1,17 +1,26 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
+import { useTodos } from '../../hooks/useTodos';
 
 
 
 export default function Todos() {
   const { user } = useContext(UserContext);
+  const [description, setDescription] = useState('');
+  const { items, setItems } = useTodos();
 
   if (!user) {
     return <Redirect to='/auth/sign-in' />;
   }
 
   return (
-    <div>hello</div>
+    <div>
+      <input
+        type='text'
+        value={description}
+      />
+      <button>Add Todo</button>
+    </div>
   );
 }
--- src/services/todos.js
+++ src/services/todos.js
@@ -1,22 +1,22 @@
-import { checkError, client } from "./client";
+import { checkError, client } from './client';
 
 export async function getTodos() {
-    const response = await client.from('todos').select();
-    return checkError(response);
+  const response = await client.from('todos').select();
+  return checkError(response);
 }
 
 export async function createTodo(description) {
-    const response = await client.from('todos').insert({ description });
+  const response = await client.from('todos').insert({ description });
 
-    return checkError(response)
+  return checkError(response);
 }
 
 export async function toggleTodo({ id, complete }) {
-    const response = await client
+  const response = await client
     .from('todos')
     .update({ complete: !complete })
     .match({ id })
     .single();
 
-    return checkError(response);
+  return checkError(response);
 }
